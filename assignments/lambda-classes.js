2f5e5c1b9d3747d9db1ec27681a9817f6265319f
donald-easter <deejayeaster@live.com>
2019-06-13 16:41:48 -0400

    completed classes, now going back and filling in details from the readme

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,12 +2,13 @@
 
 class Person {
   constructor(attr) {
-    //put something here
     this.name = attr.name;
     this.age = attr.age;
-    this.role = attr.role;
-    this.favLang = attr.favLang;
+    this.local = attr.local;
     this.favColor = attr.favColor;
+  }
+  speak() {
+    return `Hello, my name is ${this.name}, I am from ${this.local}.`;
   }
   shakeHead() {
     return `${this.name} shakes their head at Deejay.`;
@@ -17,15 +18,44 @@ class Person {
       this.name
     } shouted unto the world "MY FAVORITE COLOR IS ${this.favColor.toUpperCase()}"`;
   }
-  fellDown() {
-    return `${this.name} tripped and fell.`;
+  walkAway() {
+    return `${this.name} walks away silently.`;
   }
+}
+
+class Instructor extends Person {
+  constructor(instructorAttr) {
+    super(instructorAttr);
+    this.specialty = instructorAttr.specialty;
+    this.catchPhrase = instructorAttr.catchPhrase;
+    this.favLang = instructorAttr.favLang;
+  }
+
   langShout() {
     return `${this.name} realized how much they love ${
       this.favLang
-    } and call onto you to learn ${this.favLang}.`;
+    } and call onto you to join the legion of ${this.favLang} disciples.`;
   }
-  walkAway() {
-    return `${this.name} walks away silently.`;
+  demo(subject) {
+    return `Today we will learn all about ${subject}.`;
+  }
+  grade(student) {}
+}
+
+class Student extends Person {
+  constructor(studentAttr) {
+    super(studentAttr);
+    this.title = studentAttr.title;
+    this.coHort = studentAttr.coHort;
+    this.schedule = studentAttr.schedule;
+  }
+  fellDown() {
+    return `${this.name} tripped and fell.`;
+  }
+  cry() {
+    return `${this.name} quietly wept.`;
+  }
+  dance() {
+    return `${this.name} is dancing with joy.`;
   }
 }
