6d06900f9df4a20168b0f386fc42dbf0ff227c54
donald-easter <deejayeaster@live.com>
2019-06-13 16:13:25 -0400

    completed Person class and functions on lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,31 @@
 // CODE here for your Lambda Classes
-zip;
+
+class Person {
+  constructor(attr) {
+    //put something here
+    this.name = attr.name;
+    this.age = attr.age;
+    this.role = attr.role;
+    this.favLang = attr.favLang;
+    this.favColor = attr.favColor;
+  }
+  shakeHead() {
+    return `${this.name} shakes their head at Deejay.`;
+  }
+  colorShout() {
+    return `${
+      this.name
+    } shouted unto the world "MY FAVORITE COLOR IS ${this.favColor.toUpperCase()}"`;
+  }
+  fellDown() {
+    return `${this.name} tripped and fell.`;
+  }
+  langShout() {
+    return `${this.name} realized how much they love ${
+      this.favLang
+    } and call onto you to learn ${this.favLang}.`;
+  }
+  walkAway() {
+    return `${this.name} walks away silently.`;
+  }
+}
